934c07e43ceee7a18e64fef3eb20aa3672a35cfe
Divyansh kanaujia <kanaujiadivyansh@gmail.com>
2025-02-13 22:02:40 +0530

    1. Connected MongoDB with server

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const connectDB = require('./config/db.js')
+const cors = require('cors')
+// const authRoute = require('./routes/authRoute.js')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+connectDB()
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('User Management System API is running!')
+})
+
+// app.use('/api/auth', authRoute)
+// app.use('/api/user',userRoute)
+// app.use('/api/admin',superAdminRoute)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
