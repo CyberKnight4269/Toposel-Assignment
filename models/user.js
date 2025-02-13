d2da88d3da13900f5f8305067f2809426196fcad
Divyansh kanaujia <kanaujiadivyansh@gmail.com>
2025-02-13 22:08:37 +0530

    2. Added user schema

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+        },
+        fullName: {
+            type: String,
+            required: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+        },
+        date_of_birth: {
+            type: String,
+            required: true,
+        },
+        country: {
+            type: String,
+            required: true,
+        }
+    }
+)
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {expiresIn: '1h'})
+    return token
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    const refreshToken = jwt.sign({ id: this._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' })
+    return refreshToken;
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
